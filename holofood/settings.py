82e6e12d941c870bcbc65d96459be18b4593823b
sandyr <sandyr@ebi.ac.uk>
2022-08-05 14:56:27 +0100

    switch to default production mode in django settings

--- holofood/settings.py
+++ holofood/settings.py
@@ -40,7 +40,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", False)
 
 LOGGING = {
     "version": 1,
@@ -56,7 +56,7 @@ LOGGING = {
     },
 }
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("DATA_PORTAL_URL", "127.0.0.1")]
 
 holofood_config_env = os.getenv("HOLOFOOD_ENV_FILE")
 if holofood_config_env:
