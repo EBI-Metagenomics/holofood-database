ef165e83ec755ed108b186973b772a06b80a7d52
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2023-04-26 16:49:44 +0000

    Built site for gh-pages

--- holofood/external_apis/ena/auth.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-from requests.auth import HTTPBasicAuth
-
-
-ENA_AUTH = HTTPBasicAuth(
-    settings.HOLOFOOD_CONFIG.ena.username, settings.HOLOFOOD_CONFIG.ena.password
-)
